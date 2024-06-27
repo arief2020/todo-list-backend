8322a3980bc05c1a55316e7b4ec63db956967def
arief2020 <msyaifullahalarief@gmail.com>
2024-06-27 15:20:30 +0700

    feat: api helo chack

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
+// const express = require("express");
+// const dotenv = require("dotenv");
+
+// dotenv.config()
+// const app = express();
+// const port = process.env.APP_PORT || 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Express!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+
 const express = require("express");
-const dotenv = require("dotenv");
+const morgan = require("morgan");
+const router = require("./router/index");
+const errorHandler = require("./middleware/errorHandler");
 
-dotenv.config()
 const app = express();
-const port = process.env.APP_PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Express!");
-});
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(router);
+app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+module.exports = app;
